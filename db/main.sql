7569bf10fe0b5d0c33767b87d0dd35d97e17ce42
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-20 21:47:47 -0700

    barbones sql with 3 tables

--- db/main.sql
+++ db/main.sql
@@ -4,21 +4,41 @@ CREATE database employee_DB;
 USE employee_DB;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    department_name VARCHAR(30)
+    department_id INT AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY (department_id)
 );
+
+SELECT * FROM department;
+
+INSERT INTO department (department_name)
+VALUES('Front of House');
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,   
+    role_id INT AUTO_INCREMENT,   
     title VARCHAR(30),       
     salary DECIMAL,          
-    department_id INT
+    PRIMARY KEY (role_id),
+    CONSTRAINT
+    FOREIGN KEY department_id (role_id)
+		REFERENCES department (department_id)
+		ON DELETE CASCADE
 );
 
+INSERT INTO role (title, salary)
+VALUES ('Manager', 123000.30);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    employee_id INT AUTO_INCREMENT,
     first_name VARCHAR(30),         
-    last_name VARCHAR(30),           
-    role_id INT,                     
-    manager_id INT
+    last_name VARCHAR(30), 
+    PRIMARY KEY (employee_id),
+    CONSTRAINT
+    FOREIGN KEY role_id (employee_id)
+		REFERENCES role(role_id)
+		ON DELETE CASCADE
 );
