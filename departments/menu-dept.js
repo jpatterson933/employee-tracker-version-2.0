972bc5e6b5505cb7bfaef749757694946f05d387
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-06-30 12:53:32 -0700

    deleted depatments function from menu-dept.js and removed unneccessary require statements

--- departments/menu-dept.js
+++ departments/menu-dept.js
@@ -1,20 +1,11 @@
 const inquirer = require('inquirer');
 const connection = require('../config/connection');
-const viewDepartments = require('../departments/view-dept')
-const addDepartment = require('../departments/add-dept')
-const deleteDepartment = require('../departments/del-dep')
-const editDepartment = require('../departments/edit-dept')
-const mainMenu = require('../main-menu')
 
 class Department {
     constructor() {
         this.departments = []
 
     }
-
-
-
-
 
     menu() {
         try {
@@ -278,43 +269,5 @@ class Department {
 
 
 };
-
-const departmentsMenu = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'Departments Menu',
-                name: 'deptMenu',
-                choices: ['View Departments', 'Add Departments', 'Edit Departments', 'Delete Departments', 'Exit']
-            }
-        ])
-        .then(menuChoice => {
-            switch (menuChoice.deptMenu) {
-                case 'View Departments':
-                    console.log('You chose view departments')
-                    viewDepartments();
-                    break;
-                case 'Add Departments':
-                    console.log('You chose to add a department')
-                    addDepartment();
-                    break;
-                case 'Edit Departments':
-                    console.log('You chose to edit the departments')
-                    editDepartment();
-                    break;
-                case 'Delete Departments':
-                    console.log('You are eleminating an entire Department')
-                    deleteDepartment();
-                    break;
-                case 'Exit':
-                    console.log('Goodbye! Type node server to pull up main menu!')
-                    //need to add an exit function
-                    // return mainMenu();
-                    connection.end();
-                    break;
-            }
-        })
-}
 
 module.exports = Department;
