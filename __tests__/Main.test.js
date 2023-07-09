eb4e1b7d95493f1ae934a693a035a3f6c33013ef
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-09 14:59:42 -0700

    added a comment about previous error to file

--- __tests__/Main.test.js
+++ __tests__/Main.test.js
@@ -1,7 +1,8 @@
 const Main = require('../models/Main');
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 describe("Testing Main model build", () => {
-
+    // surely there is a better way to do this, but solves this error for now
+    /* ReferenceError: You are trying to `import` a file after the Jest environment has been torn down. From __tests__/Main.test.js. */
     afterEach(async () => {
         await sleep(2000);
     })
