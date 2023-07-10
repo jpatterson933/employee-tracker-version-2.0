a9241ede10a17afc874724b65d7837b566134932
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-10 12:55:49 -0700

    added semis to main.test.js

--- __tests__/Main.test.js
+++ __tests__/Main.test.js
@@ -7,19 +7,19 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 afterAll(() => {
     console.log("closing database connection");
     connection.end();
-})
+});
 describe("Testing Main model build", () => {
 
     // surely there is a better way to do this, but solves this error for now
     /* ReferenceError: You are trying to `import` a file after the Jest environment has been torn down. From __tests__/Main.test.js. */
     afterEach(async () => {
         await sleep(2000);
-    })
+    });
     test("expect instance of Main class", () => {
         const model = new Main();
         expect(typeof (model)).toBe("object");
     });
-})
+});
 
 describe("testing getRoles() method", () => {
     test("expect the roleId and roleName to be arrays", () => {
@@ -30,14 +30,14 @@ describe("testing getRoles() method", () => {
                 callback(null, [
                     { roleId: 1, title: 'Role 1' },
                     { roleId: 2, title: 'Role 2' }
-                ])
-            })
+                ]);
+            });
         const model = new Main();
         model.getRoles();
         expect(Array.isArray(model.roleId)).toBe(true);
         expect(Array.isArray(model.roleName)).toBe(true);
-    })
-})
+    });
+});
 
 describe("Testing getEmployees() method", () => {
     test("expect the first_name to be an array", () => {
@@ -48,14 +48,14 @@ describe("Testing getEmployees() method", () => {
                 callback(null, [
                     { first_name: "Employee1" },
                     { first_name: "Employee2" }
-                ])
-            })
+                ]);
+            });
 
         const model = new Main();
         model.getEmployees();
         expect(Array.isArray(model.employees)).toBe(true);
-    })
-})
+    });
+});
 
 describe("Testing getDepartments() method", () => {
     test("expect the department and departmentId to be an array", () => {
@@ -66,12 +66,12 @@ describe("Testing getDepartments() method", () => {
                 callback(null, [
                     { department_name: "Department One", department_id: 1 },
                     { department_name: "Department Two", department_id: 2 },
-                ])
-            })
+                ]);
+            });
 
         const model = new Main();
         model.getDepartments();
         expect(Array.isArray(model.departmentId)).toBe(true);
         expect(Array.isArray(model.departments)).toBe(true);
-    })
-})
+    });
+});
