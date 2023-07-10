a23126eabb9d4a398342678be4e3a5dfb0d3e22c
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-10 10:11:02 -0700

    main.test.js currently running without errors

--- __tests__/Main.test.js
+++ __tests__/Main.test.js
@@ -1,15 +1,48 @@
+// require('iconv-lite').encodingExists('foo')
+const mysql = require('mysql2');
+require('dotenv').config()
+
+const connection = require('../config/connection');
+
 const Main = require('../models/Main');
+
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// beforeEach(() => {
+//     mysql.createConnection({
+//         host: 'localhost',
+//         // Your port, if not 3306
+//         port: 3306,
+//         // we grab our stored variables from .env file
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME,
+//       });
+//       console.log("setting up database connection")
+// })
+
+afterEach(() => {
+    console.log("closing database connection")
+    connection.end();
+})
 describe("Testing Main model build", () => {
+    
     // surely there is a better way to do this, but solves this error for now
     /* ReferenceError: You are trying to `import` a file after the Jest environment has been torn down. From __tests__/Main.test.js. */
     afterEach(async () => {
         await sleep(2000);
     })
     test("Can instantiate Main model instance", async () => {
-        
-        const model = await new Main();
-        expect(typeof(model)).toBe("object");
+            const model = await new Main();
+            expect(typeof(model)).toBe("object");
     });
 
+
+})
+
+describe("testing getRoles() method", () => {
+    test("expect the roleId and roleName arrays to fille up", async () => {
+        const model = await new Main();
+        
+    })
 })
